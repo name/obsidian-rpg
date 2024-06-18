371d873d7b8d8e4a1ccf5169344fa6e8d1fbff35
charlie <charlie@maddex.dev>
2024-06-18 23:43:41 +0100

    initial working version

--- main.ts
+++ main.ts
@@ -1,134 +1,135 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface RPGSettings {
+  lastLoad: number;
+  level: number;
+  xp: number;
+  xpPerSecond: number;
+  xpPerFile: number;
+  currentSessionDuration: number;
+  displayStaringTimeAsHumanString: boolean;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const SETTINGS: RPGSettings = {
+  lastLoad: Date.now(),
+  level: 1,
+  xp: 0,
+  xpPerSecond: 1,
+  xpPerFile: 50,
+  currentSessionDuration: 0,
+  displayStaringTimeAsHumanString: true
+};
+
+export default class RPG extends Plugin {
+  settings: RPGSettings;
+  statsStatusBar: HTMLElement;
+  xpStatusBar: HTMLElement;
+  levelStatusBar: HTMLElement;
+  fileCount: number = 0;
+
+  async onload() {
+    await this.loadSettings();
+    this.settings.lastLoad = Date.now();
+    this.settings.currentSessionDuration = 0;
+    this.statsStatusBar = this.addStatusBarItem();
+
+    this.registerInterval(window.setInterval(() => {
+        this.settings.currentSessionDuration += 1000;
+        this.settings.xp += this.settings.xpPerSecond;
+        this.updateStats();
+        this.saveSettings();
+    }, 1000));
+
+    this.app.workspace.onLayoutReady(() => {
+      this.registerEvent(this.app.vault.on('create', () => {
+        new Notice(`You gained ${this.settings.xpPerFile} XP for creating a file!`);
+        this.updateStats();
+      }));
+    });
+
+    this.registerInterval(window.setInterval(() => {
+      this.saveSettings();
+    }, 600000));
+
+    this.addSettingTab(new RPGSettingsTab(this.app, this));
+  }
+
+  onunload() {
+    this.saveSettings();
+  }
+
+  async loadSettings() {
+    this.settings = Object.assign(SETTINGS, await this.loadData());
+  }
+
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
+
+  updateStats() {
+    this.fileCount = this.app.vault.getFiles().length;
+    const totalXP = this.settings.xp + (this.fileCount * this.settings.xpPerFile);
+    const currentLevel = Math.floor(totalXP / 600) + 1;
+    if (currentLevel > this.settings.level) {
+      this.settings.level = currentLevel;
+      new Notice(`You leveled up to level ${currentLevel}!`);
+    }
+    if (currentLevel < this.settings.level) {
+      this.settings.level = currentLevel;
+      new Notice(`You leveled down to level ${currentLevel}!`);
+    }
+    this.statsStatusBar.setText(`[${this.settings.level}] ${totalXP} XP gained`);
+  }
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+class RPGSettingsTab extends PluginSettingTab {
+  plugin: RPG;
 
-	async onload() {
-		await this.loadSettings();
+  constructor(app: App, plugin: RPG) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+  display(): void {
+    let { containerEl } = this;
+    containerEl.empty();
+    containerEl.createEl('h3', { text: 'RPG settings' });
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+    new Setting(containerEl)
+      .setName('XP gained per second')
+      .setDesc('Set the amount of XP gained for each second spent in the vault.')
+      .addText((text) =>
+        text
+          .setValue(this.plugin.settings.xpPerSecond.toString())
+          .onChange((value) => {
+            this.plugin.settings.xpPerSecond = parseInt(value);
+            this.plugin.saveSettings();
+          })
+      );
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+    new Setting(containerEl)
+      .setName('XP gained per file')
+      .setDesc('Set the amount of XP gained for each file in the vault.')
+      .addText((text) =>
+        text
+          .setValue(this.plugin.settings.xpPerFile.toString())
+          .onChange((value) => {
+            this.plugin.settings.xpPerFile = parseInt(value);
+            this.plugin.saveSettings();
+          })
+      );
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+      new Setting(containerEl)
+      .setName('Reset XP and level')
+      .setDesc('WARNING: Enabling or Disabling this option will reset your XP and level to 0.')
+      .addToggle((toggle) =>
+        toggle
+          .setValue(this.plugin.settings.displayStaringTimeAsHumanString)
+          .onChange((value) => {
+            this.plugin.settings.xp = 0;
+            this.plugin.settings.level = 1;
+            this.plugin.saveSettings();
+          })
+      );
+  }
 }
